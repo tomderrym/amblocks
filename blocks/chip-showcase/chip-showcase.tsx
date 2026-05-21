dfc01e7da9adccc87c35f7d8c8c4eb13f41fc34f
tomderrym <tomderrymarionmantilla@gmail.com>
2026-05-22 02:26:09 +0800

    Add chip-showcase.tsx

---
+++ blocks/chip-showcase/chip-showcase.tsx
@@ -0,0 +1,32 @@
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonChip, IonLabel, IonAvatar } from '../../ionic-replica';
+
+export default function ChipShowcase() {
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardSubtitle>Chips</IonCardSubtitle>
+        <IonCardTitle>Pill Components</IonCardTitle>
+      </IonCardHeader>
+      <IonCardContent>
+        <div className="flex flex-wrap gap-2">
+          <IonChip>Default Chip</IonChip>
+          <IonChip color="primary">Primary Chip</IonChip>
+          <IonChip color="success">Success Chip</IonChip>
+          <IonChip color="danger">Danger</IonChip>
+        </div>
+        <div className="flex flex-wrap gap-2 mt-4">
+          <IonChip outline color="primary">Outline Primary</IonChip>
+          <IonChip outline color="danger">Outline Danger</IonChip>
+        </div>
+        <div className="flex flex-wrap gap-2 mt-4">
+          <IonChip>
+            <IonAvatar className="w-6 h-6 mr-1 -ml-1">
+              <img src="https://i.pravatar.cc/150?img=33" alt="avatar" className="w-full h-full object-cover" />
+            </IonAvatar>
+            <IonLabel>Avatar Chip</IonLabel>
+          </IonChip>
+        </div>
+      </IonCardContent>
+    </IonCard>
+  );
+}
