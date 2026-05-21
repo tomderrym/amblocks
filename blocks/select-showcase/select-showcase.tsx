3434f17aaaac27ed335bbfb6c4a8d871577d3de9
tomderrym <tomderrymarionmantilla@gmail.com>
2026-05-22 02:27:08 +0800

    Add select-showcase.tsx

---
+++ blocks/select-showcase/select-showcase.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonSelect, IonSelectOption } from '../../ionic-replica';
+
+export default function SelectShowcase() {
+  const [value, setValue] = useState('apples');
+
+  return (
+    <IonCard>
+      <IonCardHeader>
+        <IonCardSubtitle>Select</IonCardSubtitle>
+        <IonCardTitle>Pickers</IonCardTitle>
+      </IonCardHeader>
+      <IonCardContent>
+        <IonSelect 
+          label="Favorite Fruit" 
+          value={value} 
+          onChange={(e: any) => setValue(e.target.value)}
+        >
+          <IonSelectOption value="apples">Apples</IonSelectOption>
+          <IonSelectOption value="oranges">Oranges</IonSelectOption>
+          <IonSelectOption value="bananas">Bananas</IonSelectOption>
+          <IonSelectOption value="grapes">Grapes</IonSelectOption>
+        </IonSelect>
+
+        <p className="mt-4 text-sm text-gray-500">Selected value: {value}</p>
+      </IonCardContent>
+    </IonCard>
+  );
+}
