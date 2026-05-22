e5b74043482575609770801948d2a2ab3a8a4269
tomderrym <tomderrymarionmantilla@gmail.com>
2026-05-22 14:16:08 +0800

    Updating DatetimeShowcase/DatetimeShowcase.js

---
+++ blocks/DatetimeShowcase/DatetimeShowcase.js
@@ -0,0 +1,34 @@
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonDatetime } from "../../ionic-replica";
+function DatetimeShowcase() {
+  return <div className="">
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Datetime Picker</IonCardTitle>
+          <IonCardSubtitle>Inline iOS Calendar</IonCardSubtitle>
+        </IonCardHeader>
+        <IonCardContent>
+           <p className="mb-4">
+            Notice the distinct iOS styling for the calendar, including the rounded selection 
+            indicators, the specific scroll snapping for time wheels, and the compact presentation.
+          </p>
+          <div className="flex justify-center flex-col items-center">
+            <IonDatetime presentation="date" />
+          </div>
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Time Picker</IonCardTitle>
+        </IonCardHeader>
+        <IonCardContent>
+          <div className="flex justify-center flex-col items-center">
+            <IonDatetime presentation="time" />
+          </div>
+        </IonCardContent>
+      </IonCard>
+    </div>;
+}
+export {
+  DatetimeShowcase as default
+};
